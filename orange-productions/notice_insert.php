79306e6c2a90aec8a72b824871bc9840317aa03b
iamchosenlee <iamchosen@naver.com>
2019-06-04 22:59:26 +0900

    Update notice_insert.php
    공지사항 입력 Transaction 기능 구현

--- orange-productions/notice_insert.php
+++ orange-productions/notice_insert.php
@@ -11,15 +11,21 @@ $project_id = $_POST['project_id'];
 $added_date = date("Y-m-d H:i:s");
 
 
+mysqli_query($conn, "set autocommit = 0"); //autocommit 해제
+mysqli_query($conn, "set transaction isolation level serializable = 0"); //isolation level 설정
+mysqli_query($conn, "begin"); //begin a transaction
+
 $ret = mysqli_query($conn, "insert into notice (title, content, emp_id, project_id, added_date) values('$title', '$content', '$emp_id', '$project_id', '$added_date')");
 if(!$ret)
 {
+	mysqli_query($conn, "rollback"); //notice insert 실패, 이전으로 rollback
 	echo mysqli_error($conn);
-    msg('Query Error : '.mysqli_error($conn));
+    	msg('Query Error : '.mysqli_error($conn));
 }
 else
 {
-    s_msg ('성공적으로 입력 되었습니다');
+    	mysqli_query($conn, "commit");  // 공지사항 등록 성공, 수행 내역 commit
+	s_msg ('성공적으로 입력 되었습니다');
     echo "<meta http-equiv='refresh' content='0;url=notice_list.php'>";
 }
 
