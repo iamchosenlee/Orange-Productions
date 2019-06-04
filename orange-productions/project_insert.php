371fc98fc91fbf6240f579843d7ddd9c2907166c
iamchosenlee <iamchosen@naver.com>
2019-06-04 23:18:48 +0900

    Update project_insert.php
    프로젝트 입력 Transaction 기능 구현

--- orange-productions/project_insert.php
+++ orange-productions/project_insert.php
@@ -12,14 +12,19 @@ $director = $_POST['director'];
 $emp_id = $_POST['emp_id'];
 $synopsis = $_POST['synopsis'];
 
+mysqli_query($conn, "set autocommit = 0"); //autocommit 해제
+mysqli_query($conn, "set transaction isolation level serializable = 0"); //isolation level 설정
+mysqli_query($conn, "begin"); //begin a transaction
 $ret = mysqli_query($conn, "insert into project (project_name, genre, year, director, emp_id, synopsis, stage_no) values('$project_name', '$genre', '$year', '$director', '$emp_id', '$synopsis', '$stage_no')");
 if(!$ret)
 {
+	mysqli_query($conn, "rollback"); //project insert 실패, 이전으로 rollback
 	echo mysqli_error($conn);
     msg('Query Error : '.mysqli_error($conn));
 }
 else
 {
+	mysqli_query($conn, "commit");  // 프로젝트 입력 성공, 수행 내역 commit
     s_msg ('성공적으로 입력 되었습니다');
     echo "<meta http-equiv='refresh' content='0;url=project_list.php'>";
 }
